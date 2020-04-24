f77e753db6154a95790a5f400fdc86c4ad500ac1
Irene Jaramillo <Irene.Jaramillo@alaskaair.com>
2020-04-23 18:35:24 -0700

    Week 2 React Css changes and new components added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Directory from './components/DirectoryComponent';
 import './App.css';
+import { CAMPSITES } from './shared/campsites';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            campsites: CAMPSITES
+        };
+    }
     render() {
         return (
             <div className="App">
                 <Navbar dark color="primary">
-                <div className="container">
-                    <NavbarBrand href="/">NuCamp</NavbarBrand>
-                </div>
+                    <div className="contain.er">
+                        <NavbarBrand href="/">NuCamp</NavbarBrand>
+                    </div>
                 </Navbar>
+                <Directory campsites={this.state.campsites} />
             </div>
         );
     }
 }
 
 export default App;
+//git push -u origin master
