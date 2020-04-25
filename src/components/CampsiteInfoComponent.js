20cbe912897d1232e458545d630d1970a4b1d28a
Irene Jaramillo <Irene.Jaramillo@alaskaair.com>
2020-04-25 14:47:48 -0700

    Week 2 React Whsp 2 Cmplt

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -16,28 +16,50 @@ class CampsiteInfo extends Component {
 
     renderCampsite(campsite) {
         if (campsite) {
-        return (
-            <div><Row>
-            <div className="col-md-5 m-1">
-                <Card>
-                    <CardImg top src={campsite.image} alt={campsite.name} />
-                    <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
-                </Card>
-            </div>
-            </Row></div>
-        );
+            return (
+                    <div className="col-md-5 m-1">
+                        <Card>
+                            <CardImg top src={campsite.image} alt={campsite.name} />
+                            <CardBody>
+                                <CardTitle>{campsite.name}</CardTitle>
+                                <CardText>{campsite.description}</CardText>
+                            </CardBody>
+                        </Card>
+                    </div>
+            );
+        }
+        return <div />;
+    }
+
+    renderComments(c) {
+        //const array1 = comment.comments;
+        if (c) {
+            return (<div className="col-md-5 m-1">
+                <h4>Comments</h4>
+                {c.comments.map(comment => {
+                    return (
+                        <Row className="spacer">
+                            {comment.text}
+                            <br></br>
+                            --{comment.author},  
+                             {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                             <br></br>
+                        </Row>
+                    )
+                })}
+            </div>);
         }
         return <div />;
     }
 
     render() {
-        
+
         return (
             <div className="container">
+                <Row md="5">
                 {this.renderCampsite(this.props.campsites)}
+                {this.renderComments(this.props.campsites)}
+                </Row>
             </div>
         );
     }
