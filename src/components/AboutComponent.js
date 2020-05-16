c0add3f955e10138ac190c569f2e053e723b8320
Irene Jaramillo <Irene.Jaramillo@alaskaair.com>
2020-05-16 12:25:22 -0700

    I got partner to work in AboutComponent.js

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
 function RenderPartner({ partner }) {
     if (partner) {
@@ -24,9 +25,8 @@ function RenderPartner({ partner }) {
     }
 }
 
-function About(props) {
-
-    const partners = props.partners.map(partner => {
+function PartnerList(props) {
+    const partners = props.partners.partners.map(partner => {
         return (
             <Media tag="li" key={partner.id}>
                 <RenderPartner partner={partner} />
@@ -34,6 +34,30 @@ function About(props) {
             /*} <h5>{partner.name}</h5>*/
         );
     });
+
+        if (props.partners.isLoading) {
+            return (
+                <Loading />
+            );
+        }
+        if (props.partners.errMess) {
+            return (
+                <h4>{props.partners.errMess}</h4>
+            );
+        }
+        return (
+            <div className="col mt-4">
+                <div className="col mt-4">
+                    <Media list>
+                        {partners}
+                    </Media>
+                </div>
+            </div>
+        )
+}
+
+function About(props) {
+
 
     return (
         <div className="container">
@@ -87,11 +111,7 @@ function About(props) {
                 <div className="col-12">
                     <h3>Community Partners</h3>
                 </div>
-                <div className="col mt-4">
-                    <Media list>
-                        {partners}
-                    </Media>
-                </div>
+                <PartnerList partners={props.partners}/>
             </div>
         </div>
     );
